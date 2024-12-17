5513bdfe2744ce571daf201ed80e2b31479d41e7
Colin de Vries <colindv2000@gmail.com>
2024-12-17 15:27:48 +0100

    Better reauth

--- custom_components/philips_pet_series/config_flow.py
+++ custom_components/philips_pet_series/config_flow.py
@@ -13,7 +13,14 @@ from homeassistant.data_entry_flow import FlowResult
 from petsseries import PetsSeriesClient
 from petsseries.auth import AuthError
 
-from .const import DOMAIN, CONF_ACCESS_TOKEN, CONF_REFRESH_TOKEN, CONF_TUYA_CLIENT_ID, CONF_TUYA_IP, CONF_TUYA_LOCAL_KEY
+from .const import (
+    DOMAIN,
+    CONF_ACCESS_TOKEN,
+    CONF_REFRESH_TOKEN,
+    CONF_TUYA_CLIENT_ID,
+    CONF_TUYA_IP,
+    CONF_TUYA_LOCAL_KEY,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,6 +34,13 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
     }
 )
 
+STEP_REAUTH_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_ACCESS_TOKEN): str,
+        vol.Required(CONF_REFRESH_TOKEN): str,
+    }
+)
+
 
 async def validate_input(hass: HomeAssistant, data: Dict[str, Any]) -> Dict[str, Any]:
     """Validate the user input allows us to connect."""
@@ -35,7 +49,6 @@ async def validate_input(hass: HomeAssistant, data: Dict[str, Any]) -> Dict[str,
     refresh_token = data[CONF_REFRESH_TOKEN]
     tuya_credentials = None
 
-    # Check if Tuya credentials are provided
     if all(
         key in data and data[key]
         for key in (CONF_TUYA_CLIENT_ID, CONF_TUYA_IP, CONF_TUYA_LOCAL_KEY)
@@ -82,7 +95,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user",
+                data_schema=STEP_USER_DATA_SCHEMA,
+                description_placeholders={
+                    "login_url": "https://www.home.id/find-appliance"
+                },
             )
 
         errors = {}
@@ -105,10 +122,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=STEP_USER_DATA_SCHEMA,
             errors=errors,
+            description_placeholders={
+                "login_url": "https://www.home.id/find-appliance"
+            },
         )
 
     async def async_step_reauth(self, entry_data: Dict[str, Any]) -> FlowResult:
         """Handle configuration reauthentication."""
+        self._entry = entry_data
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
@@ -118,17 +139,63 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is None:
             return self.async_show_form(
                 step_id="reauth_confirm",
-                description_placeholders={"title": self.context["title"]},
+                description_placeholders={
+                    "login_url": "https://www.home.id/find-appliance"
+                },
                 data_schema=vol.Schema({}),
             )
 
-        # If there are credentials to update, you can prompt the user to re-enter them here
-        return await self.async_step_user()
+        return await self.async_step_reauth_update()
+
+    async def async_step_reauth_update(
+        self, user_input: Optional[Dict[str, Any]] = None
+    ) -> FlowResult:
+        """Prompt the user to re-enter credentials during re-authentication."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_update",
+                data_schema=STEP_REAUTH_DATA_SCHEMA,
+                description_placeholders={
+                    "login_url": "https://www.home.id/find-appliance"
+                },
+            )
+
+        errors = {}
+
+        updated_data = self._entry.copy()
+        updated_data[CONF_ACCESS_TOKEN] = user_input[CONF_ACCESS_TOKEN]
+        updated_data[CONF_REFRESH_TOKEN] = user_input[CONF_REFRESH_TOKEN]
+
+        try:
+            info = await validate_input(self.hass, updated_data)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except InvalidTuyaSupport:
+            errors["base"] = "invalid_tuya_support"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            self.hass.config_entries.async_update_entry(
+                self._entry, data=updated_data
+            )
+            return self.async_create_entry(title=info["title"], data=updated_data)
+
+        return self.async_show_form(
+            step_id="reauth_update",
+            data_schema=STEP_REAUTH_DATA_SCHEMA,
+            errors=errors,
+            description_placeholders={
+                "login_url": "https://www.home.id/find-appliance"
+            },
+        )
 
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
-    
+
     def __init__(self, message: str = "Cannot connect") -> None:
         """Initialize the error."""
         super().__init__(message)
@@ -136,7 +203,7 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
-    
+
     def __init__(self, message: str = "Invalid auth") -> None:
         """Initialize the error."""
         super().__init__(message)
